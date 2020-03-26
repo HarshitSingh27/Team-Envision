b217ed004f6a35209b28b2ae8f982be6318dd50c
HarshitSingh27 <harshitsingh2727@gmail.com>
2020-03-26 21:34:26 +0530

    sess1

---
+++ app.js
@@ -0,0 +1,27 @@
+
+
+var json=[
+    {
+    name:"Kohli",
+    score:"183"
+},
+{
+    name:"Rohit",
+    score:"264"
+},
+{
+    name:"Sachin",
+    score:"200"
+},
+{
+    name:"Dhoni",
+    score:"183"
+},
+
+
+]
+
+ json.forEach(json => {
+   console.log(json.name);
+ });
+
